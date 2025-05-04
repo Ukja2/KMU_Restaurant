4bceefe47252d4521aacd8ac97108666514e2e47
Ukja2 <jaewooklee739@gmail.com>
2025-05-04 13:39:22 +0900

    Service 내부에 객체 주입 및 저장 메서드 추가

--- src/main/java/com/example/restaurant_review/repository/UserRepository.java
+++ src/main/java/com/example/restaurant_review/repository/UserRepository.java
@@ -4,7 +4,7 @@ import com.example.restaurant_review.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 
-// JpaRepository<엔티티, 기본키 타입>
+// JpaRepository<엔티티, 기본키 타입>, , DB와 상호작용, SQL 없이 DB 기능을 편리하게 사용하는 인터페이스
 public interface UserRepository extends JpaRepository<User, Long> {
 
 
---
+++ src/main/java/com/example/restaurant_review/service/UserService.java
@@ -0,0 +1,23 @@
+package com.example.restaurant_review.service;
+
+//User 엔티티
+import com.example.restaurant_review.domain.User;
+//JPA인터페이스
+import com.example.restaurant_review.repository.UserRepository;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+
+    //생성자 DI
+    public UserService(UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+
+    public User Usersave(User user){
+        return userRepository.save(user);
+    }
+
+}
